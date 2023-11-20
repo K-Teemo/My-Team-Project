3c0632d3094d16295e5c3723c10ec85cd510a9c6
K-Teemo <the1687@gmail.com>
2023-11-20 21:12:55 +0900

    feat: mysql 연결, dotenv 패키지 추가, 시퀄라이즈 초기 세팅

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ import {SERVER_PORT} from './constants/app.constant.js';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) : void => {
-  res.send('Hello World!');
+app.get(path:'/', (req, res) : void => {
+  res.send(body:'Hello World!');
 });
 
 app.listen(port, () : void => {
-  console.log(`Example app listening on port ${SERVER_PORT}`);
+  console.log(message : `Example app listening on port ${SERVER_PORT}`);
 });
